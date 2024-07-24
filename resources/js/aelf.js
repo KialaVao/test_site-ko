15e4c9f9a8446b63836ff60f972f78dc8324ce55
KialaVao <vaoandria@gmail.com>
2024-07-24 16:10:04 +0200

    update aelf.js

--- resources/js/aelf.js
+++ resources/js/aelf.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Fonction pour obtenir la date actuelle au format YYYY-MM-DD
     function getCurrentDate() {
         const today = new Date();
         const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0'); // Mois de 0 à 11, donc ajouter 1
+        const month = String(today.getMonth() + 1).padStart(2, '0');
         const day = String(today.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
 
-    const date = getCurrentDate(); // Obtenir la date actuelle
-    const zone = 'france'; // La zone peut être 'romain' ou une autre selon votre besoin
+    const date = getCurrentDate();
+    const zone = 'romain';
     const apiUrl = `https://api.aelf.org/v1/lectures/${date}/${zone}`;
 
     fetch(apiUrl)
@@ -17,18 +16,16 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             console.log(data); // Inspecter la structure de la réponse
             const paroleDuJour = document.getElementById("parole-du-jour");
-            if (data.lectures) {
-                let content = `<h4>${date}</h4>`;
-                for (const key in data.lectures) {
-                    if (data.lectures.hasOwnProperty(key)) {
-                        const lecture = data.lectures[key];
-                        content += `<h5>${lecture.titre}</h5>`;
-                        content += `<p>${lecture.texte}</p>`;
-                    }
-                }
+            if (data.lectures && data.lectures.evangile) {
+                const evangel = data.lectures.evangile;
+                const content = `
+                    <h4>${date}</h4>
+                    <h5>${evangel.titre}</h5>
+                    <p>${evangel.texte}</p>
+                `;
                 paroleDuJour.innerHTML = content;
             } else {
-                paroleDuJour.innerHTML = "Aucune lecture trouvée pour cette date.";
+                paroleDuJour.innerHTML = "Aucune lecture de l'évangile trouvée pour cette date.";
             }
         })
         .catch(error => {

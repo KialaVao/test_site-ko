4e240f33d6118190054b89e5e5ba2fc6ef7aef56
KialaVao <vaoandria@gmail.com>
2024-08-16 10:06:58 +0200

    update evenement files

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,25 +1,35 @@
-function openGallery(evt, galleryName) {
-    var i, tabcontent, tablinks;
+//function openGallery(evt, galleryName) {
+ //   var i, tabcontent, tablinks;
   
     // Cachez toutes les galeries
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+   // tabcontent = document.getElementsByClassName("tabcontent");
+    //for (i = 0; i < tabcontent.length; i++) {
+      //tabcontent[i].style.display = "none";
+    //}
   
     // Enlevez la classe "active" de tous les onglets
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    //tablinks = document.getElementsByClassName("tablinks");
+    //for (i = 0; i < tablinks.length; i++) {
+     // tablinks[i].className = tablinks[i].className.replace(" active", "");
+   // }
   
     // Affichez la galerie sélectionnée et ajoutez la classe "active" à l'onglet sélectionné
-    document.getElementById(galleryName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+    //document.getElementById(galleryName).style.display = "block";
+    //evt.currentTarget.className += " active";
+  //}
   
   // Afficher la première galerie par défaut
-  document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".tablinks").click();
-  });
+  //document.addEventListener("DOMContentLoaded", function() {
+   // document.querySelector(".tablinks").click();
+  //});
   
+  function openAlbum(albumId) {
+    // Sélectionnez tous les albums et cachez-les
+    var albums = document.getElementsByClassName('album-content');
+    for (var i = 0; i < albums.length; i++) {
+        albums[i].style.display = 'none';
+    }
+
+    // Affichez le contenu de l'album cliqué
+    document.getElementById(albumId).style.display = 'block';
+}

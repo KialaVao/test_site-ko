6a00a1d0224fcb60d0aa9adccffeee9444d67254
KialaVao <vaoandria@gmail.com>
2024-08-16 12:24:13 +0200

    update evenement files

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -4,21 +4,51 @@ document.addEventListener("DOMContentLoaded", function() {
     const lightbox = document.getElementById("lightbox");
     const lightboxImage = document.getElementById("lightbox-image");
     const closeButton = document.querySelector(".close");
+    const prevButton = document.getElementById("prev");
+    const nextButton = document.getElementById("next");
 
-    galleryItems.forEach(item => {
+    let currentIndex = 0;
+
+    // Afficher l'image dans le lightbox
+    function showImage(index) {
+        currentIndex = index;
+        lightbox.style.display = "block";
+        lightboxImage.src = galleryItems[currentIndex].src;
+    }
+
+    // Fermer le lightbox
+    function closeLightbox() {
+        lightbox.style.display = "none";
+    }
+
+    // Afficher l'image précédente
+    function showPrevImage() {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : galleryItems.length - 1;
+        showImage(currentIndex);
+    }
+
+    // Afficher l'image suivante
+    function showNextImage() {
+        currentIndex = (currentIndex < galleryItems.length - 1) ? currentIndex + 1 : 0;
+        showImage(currentIndex);
+    }
+
+    // Événements pour les clics sur les images de la galerie
+    galleryItems.forEach((item, index) => {
         item.addEventListener("click", function() {
-            lightbox.style.display = "block";
-            lightboxImage.src = this.src;
+            showImage(index);
         });
     });
 
-    closeButton.addEventListener("click", function() {
-        lightbox.style.display = "none";
-    });
+    // Événements pour les boutons de navigation
+    closeButton.addEventListener("click", closeLightbox);
+    prevButton.addEventListener("click", showPrevImage);
+    nextButton.addEventListener("click", showNextImage);
 
+    // Fermer le lightbox en cliquant en dehors de l'image
     lightbox.addEventListener("click", function(event) {
-        if (event.target !== lightboxImage) {
-            lightbox.style.display = "none";
+        if (event.target !== lightboxImage && event.target !== prevButton && event.target !== nextButton) {
+            closeLightbox();
         }
     });
 });

886a2ab81bbc2b5d989f2c8d0fa1bca7b034d10e
KialaVao <vaoandria@gmail.com>
2026-04-28 15:05:21 +0200

    update liturgie2-1

--- resources/js/lectures-dimanches.js
+++ resources/js/lectures-dimanches.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
     }
     
-
     const zone = "romain";
     const container = document.getElementById("lectures-dimanches");
 
